f0259152a320533b9bf14d0711468286dca5fd81
Saturnino Abril <saturnino.abril@gmail.com>
2020-05-14 18:37:37 +0800

    enable LDAP tests

--- e2e/cypress/integration/enterprise/ldap_group/groups_assign_roles_spec.js
+++ e2e/cypress/integration/enterprise/ldap_group/groups_assign_roles_spec.js
@@ -7,7 +7,7 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Group: @enterprise @ldap_group
+// Group: @enterprise @ldap @ldap_group
 
 // # Function to get all the teams associated to group and unlink them
 const getTeamsAssociatedToGroupAndUnlink = (groupId) => {
@@ -57,7 +57,9 @@ describe('System Console', () => {
         cy.apiUpdateConfig({LdapSettings: {Enable: true}});
 
         // # Check and run LDAP Sync job
-        cy.checkRunLDAPSync();
+        if (Cypress.env('runLDAPSync')) {
+            cy.checkRunLDAPSync();
+        }
     });
 
     it('MM-20058 - System Admin can map roles to teams and channels via group configuration page', () => {
