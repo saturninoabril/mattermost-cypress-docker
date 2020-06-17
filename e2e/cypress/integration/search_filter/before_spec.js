cf6e9d1b6f689a7190ec2430ccf69e0e761ccd1e
Saturnino Abril <saturninoabril@saturninoabril.local>
2020-06-17 12:25:50 +0800

    update

--- e2e/cypress/integration/search_filter/before_spec.js
+++ e2e/cypress/integration/search_filter/before_spec.js
@@ -10,83 +10,31 @@
 // Stage: @prod
 // Group: @search_date_filter
 
-import * as TIMEOUTS from '../../fixtures/timeouts';
 import {getAdminAccount} from '../../support/env';
-import {getRandomId} from '../../utils';
 
-import {getMsAndQueryForDate, searchAndValidate} from './helpers';
+import {
+    getTestMessages,
+    searchAndValidate,
+    setupTestData,
+} from './helpers';
 
 describe('SF15699 Search Date Filter - before', () => {
-    // Store unique suffix
-    const commonText = getRandomId();
-
-    // Setup Messages
-    const todayMessage = `1st Today's message ${commonText}`;
-    const firstMessage = `5th First message ${commonText}`;
-    const secondMessage = `3rd Second message ${commonText}`;
-    const firstOffTopicMessage = `4th Off topic 1 ${commonText}`;
-    const secondOffTopicMessage = `2nd Off topic 2 ${commonText}`;
-
-    // Store messages in expected order they'd appear
-    const allMessagesInOrder = [
-        todayMessage,
-        secondOffTopicMessage,
-        secondMessage,
-        firstOffTopicMessage,
+    const testData = getTestMessages();
+    const {
+        commonText,
+        allMessagesInOrder,
+        secondDateEarly,
         firstMessage,
-    ];
-
-    // Get dates for query and in ms for usage below
-    const firstDateEarly = getMsAndQueryForDate(Date.UTC(2018, 5, 5, 9, 30)); // June 5th, 2018 @ 9:30am
-    const firstDateLater = getMsAndQueryForDate(Date.UTC(2018, 5, 5, 9, 45)); // June 5th, 2018 @ 9:45am
-    const secondDateEarly = getMsAndQueryForDate(Date.UTC(2018, 9, 15, 13, 15)); // October 15th, 2018 @ 1:15pm
-    const secondDateLater = getMsAndQueryForDate(Date.UTC(2018, 9, 15, 13, 25)); // October 15th, 2018 @ 1:25pm
-
-    const baseUrl = Cypress.config('baseUrl');
+        firstOffTopicMessage,
+    } = testData;
     const admin = getAdminAccount();
-    let newAdmin;
+    let anotherAdmin;
 
     before(() => {
-        // # Change timezone to UTC so we are in sync with the backend
-        cy.apiPatchMe({timezone: {automaticTimezone: '', manualTimezone: 'UTC', useAutomaticTimezone: 'false'}});
+        cy.apiInitSetup({userPrefix: 'other-admin'}).then(({team, user}) => {
+            anotherAdmin = user;
 
-        cy.apiInitSetup().then(({team}) => {
-            // # Create another admin user so we can create posts from another user
-            cy.apiCreateUser().then(({user: user1}) => {
-                newAdmin = user1;
-                cy.externalRequest({user: admin, method: 'put', baseUrl, path: `users/${newAdmin.id}/roles`, data: {roles: 'system_user system_admin'}});
-
-                // # Visit town-square
-                cy.visit(`/${team.name}/channels/town-square`);
-
-                // # Create a post from today
-                cy.get('#postListContent', {timeout: TIMEOUTS.LARGE}).should('be.visible');
-                cy.postMessage(todayMessage).wait(1000);
-
-                cy.apiAddUserToTeam(team.id, newAdmin.id).then(() => {
-                    // cy.visit(`/${team.name}/channels/town-square`);
-                    cy.getCurrentChannelId().then((townSquareChannelId) => {
-                        // cy.externalRequest({user: admin, method: 'put', baseUrl, path: `users/${user.id}/roles`, data: {roles: 'system_user system_admin'}});
-
-                        // Post message as new admin to Town Square
-                        cy.postMessageAs({sender: newAdmin, message: firstMessage, channelId: townSquareChannelId, createAt: firstDateEarly.ms}).wait(1000);
-
-                        // Post message as sysadmin to Town Square
-                        cy.postMessageAs({sender: admin, message: secondMessage, channelId: townSquareChannelId, createAt: secondDateEarly.ms});
-
-                        // # Create messages at same dates in Off Topic channel
-                        cy.visit(`/${team.name}/channels/off-topic`);
-
-                        cy.getCurrentChannelId().then((offTopicChannelId) => {
-                            // Post message as sysadmin to off topic
-                            cy.postMessageAs({sender: admin, message: firstOffTopicMessage, channelId: offTopicChannelId, createAt: firstDateLater.ms}).wait(1000);
-
-                            // Post message as new admin to off topic
-                            cy.postMessageAs({sender: newAdmin, message: secondOffTopicMessage, channelId: offTopicChannelId, createAt: secondDateLater.ms});
-                        });
-                    });
-                });
-            });
+            setupTestData(testData, {team, admin, anotherAdmin});
         });
     });
 
@@ -99,7 +47,7 @@ describe('SF15699 Search Date Filter - before', () => {
     });
 
     it('can be used in conjunction with "from:"', () => {
-        searchAndValidate(`before:${secondDateEarly.query} from:${newAdmin.username} ${commonText}`, [firstMessage]);
+        searchAndValidate(`before:${secondDateEarly.query} from:${anotherAdmin.username} ${commonText}`, [firstMessage]);
     });
 
     it('using a date from the future shows results', () => {
--- e2e/cypress/integration/search_filter/helpers.js
+++ e2e/cypress/integration/search_filter/helpers.js
@@ -8,6 +8,7 @@
 // ***************************************************************
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
+import {getRandomId} from '../../utils';
 
 export function searchAndValidate(query, expectedResults = []) {
     cy.reload();
@@ -40,4 +41,95 @@ export function getMsAndQueryForDate(date) {
         ms: date,
         query: new Date(date).toISOString().split('T')[0],
     };
+}
+
+export function getTestMessages() {
+    const commonText = getRandomId();
+
+    // Setup Messages
+    const todayMessage = `1st Today's message ${commonText}`;
+    const firstMessage = `5th First message ${commonText}`;
+    const secondMessage = `3rd Second message ${commonText}`;
+    const firstOffTopicMessage = `4th Off topic 1 ${commonText}`;
+    const secondOffTopicMessage = `2nd Off topic 2 ${commonText}`;
+
+    const allMessagesInOrder = [
+        todayMessage,
+        secondOffTopicMessage,
+        secondMessage,
+        firstOffTopicMessage,
+        firstMessage,
+    ];
+
+    return {
+        commonText,
+        allMessagesInOrder,
+        todayMessage,
+        firstMessage,
+        secondMessage,
+        firstOffTopicMessage,
+        secondOffTopicMessage,
+
+        // Get dates for query and in ms for usage below
+        firstDateEarly: getMsAndQueryForDate(Date.UTC(2018, 5, 5, 9, 30)), // June 5th, 2018 @ 9:30am
+        firstDateLater: getMsAndQueryForDate(Date.UTC(2018, 5, 5, 9, 45)), // June 5th, 2018 @ 9:45am
+        secondDateEarly: getMsAndQueryForDate(Date.UTC(2018, 9, 15, 13, 15)), // October 15th, 2018 @ 1:15pm
+        secondDateLater: getMsAndQueryForDate(Date.UTC(2018, 9, 15, 13, 25)), // October 15th, 2018 @ 1:25pm
+    };
+}
+
+export function setupTestData(data, {team, admin, anotherAdmin}) {
+    const {
+        todayMessage,
+        firstMessage,
+        secondMessage,
+        firstOffTopicMessage,
+        secondOffTopicMessage,
+        firstDateEarly,
+        secondDateEarly,
+        firstDateLater,
+        secondDateLater,
+    } = data;
+
+    // # Create another admin user so we can create override create_at of posts
+    const baseUrl = Cypress.config('baseUrl');
+    cy.externalRequest({user: admin, method: 'put', baseUrl, path: `users/${anotherAdmin.id}/roles`, data: {roles: 'system_user system_admin'}});
+
+    // # Visit town-square
+    cy.visit(`/${team.name}/channels/town-square`);
+
+    // # Create a post from today
+    cy.get('#postListContent', {timeout: TIMEOUTS.LARGE}).should('be.visible');
+    cy.postMessage(todayMessage).wait(1000);
+
+    cy.apiGetChannelByName(team.name, 'town-square').then((townSquareRes) => {
+        const townSquareChannel = townSquareRes.body;
+
+        // # Post message as new admin to Town Square
+        cy.postMessageAs({sender: anotherAdmin, message: firstMessage, channelId: townSquareChannel.id, createAt: firstDateEarly.ms});
+
+        // # Post message as sysadmin to Town Square
+        cy.postMessageAs({sender: admin, message: secondMessage, channelId: townSquareChannel.id, createAt: secondDateEarly.ms});
+
+        cy.apiGetChannelByName(team.name, 'off-topic').then((offTopicRes) => {
+            const offTopicChannel = offTopicRes.body;
+
+            // # Post message as sysadmin to off topic
+            cy.postMessageAs({sender: admin, message: firstOffTopicMessage, channelId: offTopicChannel.id, createAt: firstDateLater.ms});
+
+            // # Post message as new admin to off topic
+            cy.postMessageAs({sender: anotherAdmin, message: secondOffTopicMessage, channelId: offTopicChannel.id, createAt: secondDateLater.ms});
+
+            // # Add 10 users and each post random messages to town-square and off-topic channels
+            Cypress._.times(10, () => {
+                cy.apiCreateUser().then(({user}) => {
+                    cy.apiAddUserToTeam(team.id, user.id).then(() => {
+                        [townSquareChannel, offTopicChannel].forEach((channel) => {
+                            cy.postMessageAs({sender: user, message: getRandomId(), channelId: channel.id});
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
