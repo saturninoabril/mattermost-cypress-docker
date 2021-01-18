6a15605c8f26d3ce2c2647179816b65b2b49966f
Saturnino Abril <saturnino.abril@gmail.com>
2021-01-19 00:35:07 +0800

    update

--- e2e/cypress/integration/enterprise/system_console/authentication_method_spec.js
+++ e2e/cypress/integration/enterprise/system_console/authentication_method_spec.js
@@ -7,7 +7,8 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Group: @enterpise @system_console @mfa
+// Stage: @prod
+// Group: @enterprise @system_console @mfa
 
 import ldapUsers from '../../../fixtures/ldap_users.json';
 import * as TIMEOUTS from '../../../fixtures/timeouts';
