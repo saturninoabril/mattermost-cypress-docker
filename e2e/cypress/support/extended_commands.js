9c803cc9a3cc431a2ea99494ae81fc91717e2d04
Saturnino Abril <saturnino.abril@gmail.com>
2021-03-04 18:27:09 +0800

    intercept cloud request

--- e2e/cypress/support/extended_commands.js
+++ e2e/cypress/support/extended_commands.js
@@ -5,10 +5,21 @@ import * as TIMEOUTS from '../fixtures/timeouts';
 
 Cypress.Commands.overwrite('reload', (originalFn, forceReload, options, duration = TIMEOUTS.THREE_SEC) => {
     originalFn(forceReload, options);
+    interceptCloudRequests();
     cy.wait(duration);
 });
 
 Cypress.Commands.overwrite('visit', (originalFn, url, options, duration = TIMEOUTS.THREE_SEC) => {
     originalFn(url, options);
+    interceptCloudRequests();
     cy.wait(duration);
 });
+
+function interceptCloudRequests() {
+    if (Cypress.config('experimentalNetworkStubbing')) {
+        cy.route2('api/v4/cloud/subscription/stats', {
+            remaining_seats: 10,
+            is_paid_tier: 'true',
+        });
+    }
+}
