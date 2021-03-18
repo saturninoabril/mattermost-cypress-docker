125b6a4a2a66c3930189f435e0c30db5c81e867f
Saturnino Abril <saturnino.abril@gmail.com>
2021-03-18 20:15:14 +0800

    update

--- e2e/cypress/integration/system_console/reporting/site_statistics_spec.js
+++ e2e/cypress/integration/system_console/reporting/site_statistics_spec.js
@@ -20,11 +20,20 @@ describe('System Console > Site Statistics', () => {
             testUser = user;
             cy.apiLogin(testUser);
             cy.visit(`/${team.name}/channels/town-square`);
+
+            // # Post a message as testUser to make them daily/monthly active
             cy.postMessage('New Daily Message');
         });
     });
 
     it('MM-T903 - Site Statistics > Deactivating a user increments the Daily and Monthly Active Users counts down', () => {
+        let totalActiveUsersInitial;
+        let dailyActiveUsersInitial;
+        let monthlyActiveUsersInitial;
+        let totalActiveUsersFinal;
+        let dailyActiveUsersFinal;
+        let monthlyActiveUsersFinal;
+
         // # Go to admin console
         goToAdminConsole();
 
@@ -34,36 +43,34 @@ describe('System Console > Site Statistics', () => {
 
         // # Get the number text and turn them into numbers
         cy.findByTestId('totalActiveUsers').invoke('text').then((totalActiveText) => {
-            const totalActiveUsersInitial = parseInt(totalActiveText, 10);
-            cy.findByTestId('dailyActiveUsers').invoke('text').then((dailyActiveText) => {
-                const dailyActiveUsersInitial = parseInt(dailyActiveText, 10);
-                cy.findByTestId('monthlyActiveUsers').invoke('text').then((monthlyActiveText) => {
-                    const monthlyActiveUsersInitial = parseInt(monthlyActiveText, 10);
+            totalActiveUsersInitial = parseInt(totalActiveText, 10);
+        });
+        cy.findByTestId('dailyActiveUsers').invoke('text').then((dailyActiveText) => {
+            dailyActiveUsersInitial = parseInt(dailyActiveText, 10);
+        });
+        cy.findByTestId('monthlyActiveUsers').invoke('text').then((monthlyActiveText) => {
+            monthlyActiveUsersInitial = parseInt(monthlyActiveText, 10);
 
-                    // # Deactivate user and reload page and then wait 2 seconds
-                    cy.externalActivateUser(testUser.id, false);
-                    cy.reload();
-                    cy.wait(TIMEOUTS.TWO_SEC);
+            // # Deactivate user and reload page and then wait 2 seconds
+            cy.externalActivateUser(testUser.id, false);
+            cy.reload();
+            cy.wait(TIMEOUTS.TWO_SEC);
+        });
 
-                    // # Get the numbers required again
-                    cy.findByTestId('totalActiveUsers').invoke('text').then((totalActiveFinalText) => {
-                        const totalActiveUsersFinal = parseInt(totalActiveFinalText, 10);
+        // # Get the numbers required again
+        cy.findByTestId('totalActiveUsers').invoke('text').then((totalActiveFinalText) => {
+            totalActiveUsersFinal = parseInt(totalActiveFinalText, 10);
+        });
+        cy.findByTestId('dailyActiveUsers').invoke('text').then((dailyActiveFinalText) => {
+            dailyActiveUsersFinal = parseInt(dailyActiveFinalText, 10);
+        });
+        cy.findByTestId('monthlyActiveUsers').invoke('text').then((monthlyActiveFinalText) => {
+            monthlyActiveUsersFinal = parseInt(monthlyActiveFinalText, 10);
 
-                        cy.findByTestId('dailyActiveUsers').invoke('text').then((dailyActiveFinalText) => {
-                            const dailyActiveUsersFinal = parseInt(dailyActiveFinalText, 10);
-
-                            cy.findByTestId('monthlyActiveUsers').invoke('text').then((monthlyActiveFinalText) => {
-                                const monthlyActiveUsersFinal = parseInt(monthlyActiveFinalText, 10);
-
-                                // * Assert that the final number is the initial number minus one
-                                expect(totalActiveUsersFinal).equal(totalActiveUsersInitial - 1);
-                                expect(dailyActiveUsersFinal).equal(dailyActiveUsersInitial - 1);
-                                expect(monthlyActiveUsersFinal).equal(monthlyActiveUsersInitial - 1);
-                            });
-                        });
-                    });
-                });
-            });
+            // * Assert that the final number is the initial number minus one
+            expect(totalActiveUsersFinal).equal(totalActiveUsersInitial - 1);
+            expect(dailyActiveUsersFinal).equal(dailyActiveUsersInitial - 1);
+            expect(monthlyActiveUsersFinal).equal(monthlyActiveUsersInitial - 1);
         });
     });
 });
