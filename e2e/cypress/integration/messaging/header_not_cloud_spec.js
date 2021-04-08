7e39ddc5fb626881c5bdfe50f0e3120504803c4c
Saturnino Abril <saturnino.abril@gmail.com>
2021-04-09 06:57:41 +0800

    update

--- e2e/cypress/integration/messaging/header_not_cloud_spec.js
+++ e2e/cypress/integration/messaging/header_not_cloud_spec.js
@@ -7,7 +7,7 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
+// Stage: to investigate
 // Group: @not_cloud @messaging
 
 describe('Header', () => {
