cc4533659b29890133b3ca63d08f6d1974e06caa
Saturnino Abril <saturnino.abril@gmail.com>
2021-04-21 17:57:00 +0800

    update

--- e2e/cypress/integration/emoji/recently_used_emoji_spec.js
+++ e2e/cypress/integration/emoji/recently_used_emoji_spec.js
@@ -7,7 +7,7 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: to investigate further
+// Stage: to further investigate
 // Group: @emoji
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
